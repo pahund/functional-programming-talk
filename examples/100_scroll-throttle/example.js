d24e941d9ef903a24a22d021058d0f10634a0d37
Patrick Hund <pahund@ebay.com>
2014-12-18 17:05:04 +0100

    scroll throttle step 3
    Change-Id: Ie56a44020809e5f41dba89dd73d445235bb415d9

--- examples/100_scroll-throttle/example.js
+++ examples/100_scroll-throttle/example.js
@@ -3,40 +3,43 @@
 (function () {
     "use strict";
 
-    function checkf(predf, callback, ms) {
-        window.setTimeout(function () {
-            if (predf()) {
-                callback();
-                return;
-            }
-            checkf(predf, callback, ms);
-        }, ms);
+    function periodicChecker(predf, ms) {
+        return function checkf(callback) {
+            window.setTimeout(function () {
+                if (predf()) {
+                    callback();
+                    return;
+                }
+                checkf(callback);
+            }, ms);
+        };
     }
 
     function afterScrollStops(func) {
         var ms = 100, // interval to perform the scroll position check
             oldX = $(document).scrollLeft(),
-            oldY = $(document).scrollTop();
+            oldY = $(document).scrollTop(),
+            scrollPosChecker;
 
-        function startf() {
+        scrollPosChecker = periodicChecker(function () {
+            var newX = $(document).scrollLeft(),
+                newY = $(document).scrollTop(),
+                stoppedScrolling = newX === oldX && newY === oldY;
+
+            oldX = newX;
+            oldY = newY;
+
+            return stoppedScrolling;
+        }, ms);
+
+        (function startf() {
             $(document).one("scroll", function () {
-                checkf(function () {
-                    var newX = $(document).scrollLeft(),
-                        newY = $(document).scrollTop(),
-                        stoppedScrolling = newX === oldX && newY === oldY;
-
-                    oldX = newX;
-                    oldY = newY;
-
-                    return stoppedScrolling;
-                }, function () {
+                scrollPosChecker(function () {
                     func();
                     startf();
-                }, ms);
+                });
             });
-        }
-
-        startf();
+        }());
     }
 
     afterScrollStops(function () {
