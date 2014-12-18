c51844b13c991d7f78cb4882f8d1d213e7581ebe
Patrick Hund <pahund@ebay.com>
2014-12-18 16:59:23 +0100

    scroll throttle step 1
    Change-Id: I0113fbf65dbe53f524bbccee1ba00b2910b37d14

--- examples/100_scroll-throttle/example.js
+++ examples/100_scroll-throttle/example.js
@@ -3,56 +3,35 @@
 (function () {
     "use strict";
 
-    function periodicChecker(predf, ms) {
-        return function checkf(callback) {
-            window.setTimeout(function () {
-                if (predf()) {
-                    callback();
-                    return;
-                }
-                checkf(callback);
-            }, ms);
-        };
-    }
-
-    function updater(updatef) {
-        var oldValue = updatef();
-        return function () {
-            var newValue = updatef();
-            if (oldValue !== newValue) {
-                oldValue = newValue;
-                return false;
-            }
-            return true;
-        };
-    }
-
-    function getScrollX() {
-        return $(document).scrollTop();
-    }
-
-    function getScrollY() {
-        return $(document).scrollLeft();
-    }
-
     function afterScrollStops(func) {
         var ms = 100, // interval to perform the scroll position check
-            scrollPosChecker,
-            xUpdater = updater(getScrollX),
-            yUpdater = updater(getScrollY);
+            oldX = $(document).scrollLeft(),
+            oldY = $(document).scrollTop();
 
-        scrollPosChecker = periodicChecker(function () {
-            return xUpdater() &&  yUpdater();
-        }, ms);
-
-        (function startf() {
+        function startf() {
             $(document).one("scroll", function () {
-                scrollPosChecker(function () {
+                checkf();
+            });
+        }
+
+        function checkf() {
+            window.setTimeout(function () {
+                var newX = $(document).scrollLeft(),
+                    newY = $(document).scrollTop();
+
+                if (newX === oldX && newY === oldY) {
                     func();
                     startf();
-                });
-            });
-        }());
+                    return;
+                }
+
+                oldX = newX;
+                oldY = newY;
+                checkf();
+            }, ms);
+        }
+
+        startf();
     }
 
     afterScrollStops(function () {
